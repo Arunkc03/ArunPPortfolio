f8e2c11c96cde9bae52594bb7e6a786b87aa8ab1
Arunkc03 <kcarun3322@gmail.com>
2026-08-24 09:54:01 +0545

    feat: add services data

---
+++ src/data/services.js
@@ -0,0 +1,1 @@
+export const services = ['Full-Stack Web Development','Responsive Website Development','REST API Development','Flutter Mobile App Development','Database Development','UI Implementation'];
